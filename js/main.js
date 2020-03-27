288ce359f7a92cc6b0e2f4bcbc4268d7f554c849
Riccardo Belli <riccardo_belli@icloud.com>
2020-03-27 20:07:36 +0100

    Completamento Ex (fix con la Table) e attivato il secondo button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 // Variabili
 var euroPerKM = 0.21;
-var scontoUnder18 = 0.8;
-var scontoOver65 = 0.6;
+var discountUnder18 = 0.8;
+var discountOver65 = 0.6;
 
 var nameUser = document.getElementById('name');
 var kmUser = document.getElementById('km');
 var ageUser = document.getElementById('age');
+var postEventContainer = document.getElementById('container-post-event');
+
+document.getElementById('clear').addEventListener("click",
+  function() {
+    nameUser.value = "";
+    kmUser.value = "";
+    ageUser.value = "";
+    postEventContainer.style.display = "none";
+  }
+)
 
 document.getElementById('generator').addEventListener("click",
   function() {
@@ -13,27 +23,28 @@ document.getElementById('generator').addEventListener("click",
     var ageChanged = ageUser.value;
     var kmChanged = kmUser.value;
 
-    var scontoBiglietto;
+    var discountTicket;
     var ticketType;
     // Calcolo costo Biglietto
-    var costoBiglietto = kmChanged * euroPerKM;
+    var ticketPrice = kmChanged * euroPerKM;
     // Calcolo biglietti con eventuali sconti
     if (ageChanged === "minorenni") {
-      scontoBiglietto = costoBiglietto * scontoUnder18;
+      discountTicket = ticketPrice * discountUnder18;
       ticketType = "Sconto Minorenni";
     } else if (ageChanged === "over65") {
-      scontoBiglietto = costoBiglietto * scontoOver65;
+      discountTicket = ticketPrice * discountOver65;
       ticketType = "Sconto Over65";
     } else {
-      scontoBiglietto = costoBiglietto;
+      discountTicket = ticketPrice;
       ticketType = "Biglietto Standard";
     }
-    
+
     // Stampa del risultato
+    postEventContainer.style.display = "block";
     document.getElementById('output-name-user').innerHTML = nameChanged;
     document.getElementById('output-offer').innerHTML = ticketType;
     document.getElementById('output-place').innerHTML = Math.floor((Math.random() * 9) + 1);
     document.getElementById('output-code').innerHTML = Math.floor((Math.random() * 99999) + 1);
-    document.getElementById('output-price').innerHTML = scontoBiglietto.toFixed(2);
+    document.getElementById('output-price').innerHTML = discountTicket.toFixed(2);
   }
 );
